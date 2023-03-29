673fc918a1a4daeddcb5c9571dba678997354810
= <alexandressoares383@gmail.com>
2023-03-29 18:07:57 -0300

    refactor: move all protocol imports to a single file

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,9 +1,12 @@
 /* eslint-disable consistent-return */
-import { HttpResponse, HttpRequest } from '../protocols/http';
+import {
+  Controller,
+  EmailValidator,
+  HttpResponse,
+  HttpRequest,
+} from '../protocols';
 import { MissingParamError, InvalidParamError } from '../errors';
 import { badRequest, serverError } from '../helpers/http-helpers';
-import { Controller } from '../protocols/controller';
-import { EmailValidator } from '../protocols/email-validator';
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator;
