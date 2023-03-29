e204ee1721d478f484c3ca15362023b9c729f6a4
= <alexandressoares383@gmail.com>
2023-03-29 14:08:38 -0300

    refactor: move sut creation to a factory helper method

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,8 +1,11 @@
 import { SignUpController } from './signup';
+import { MissingParamError } from '../errors/missing-param-error';
+
+const makeSut = (): SignUpController => new SignUpController();
 
 describe('SignUp Controller', () => {
   test('Should return 400 if no name is provided', () => {
-    const sut = new SignUpController();
+    const sut = makeSut();
     const httpRequest = {
       body: {
         email: 'any_email@gmail.com',
@@ -12,6 +15,58 @@ describe('SignUp Controller', () => {
     };
 
     const httpResponse = sut.handle(httpRequest);
+
     expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError('name'));
+  });
+
+  test('Should return 400 if no email is provided', () => {
+    const sut = makeSut();
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        password_confirmation: 'any_password',
+      },
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError('email'));
+  });
+
+  test('Should return 400 if no password is provided', () => {
+    const sut = makeSut();
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@gmail.com',
+        password_confirmation: 'any_password',
+      },
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError('password'));
+  });
+
+  test('Should return 400 if no password confirmation is provided', () => {
+    const sut = makeSut();
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@gmail.com',
+        password: 'any_password',
+      },
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(
+      new MissingParamError('password_confirmation'),
+    );
   });
 });
--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,7 +1,22 @@
-export class SignUpController {
-  handle(httpRequest: any): any {
-    return {
-      statusCode: 400,
-    };
+/* eslint-disable consistent-return */
+import { HttpResponse, HttpRequest } from '../protocols/http';
+import { MissingParamError } from '../errors/missing-param-error';
+import { badRequest } from '../helpers/http-helpers';
+import { Controller } from '../protocols/controller';
+
+export class SignUpController implements Controller {
+  handle(httpRequest: HttpRequest): HttpResponse {
+    const requireFields = [
+      'name',
+      'email',
+      'password',
+      'password_confirmation',
+    ];
+
+    for (const field of requireFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field));
+      }
+    }
   }
 }
