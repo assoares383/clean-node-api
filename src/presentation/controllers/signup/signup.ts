464f956d2ca393d9714394fac85c6eac03188d50
= <alexandressoares383@gmail.com>
2023-03-30 12:50:20 -0300

    refactor: move success http response to a helper method

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -7,7 +7,7 @@ import {
   HttpRequest,
 } from './signup-protocols';
 import { MissingParamError, InvalidParamError } from '../../errors';
-import { badRequest, serverError } from '../../helpers/http-helpers';
+import { badRequest, serverError, ok } from '../../helpers/http-helpers';
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator;
@@ -53,10 +53,7 @@ export class SignUpController implements Controller {
         password,
       });
 
-      return {
-        statusCode: 200,
-        body: account,
-      };
+      return ok(account);
     } catch (error) {
       return serverError();
     }
