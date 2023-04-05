b65fcd4ed56bb4c7e7bceab7d62022ea862820a2
= <alexandressoares383@gmail.com>
2023-04-05 19:30:22 -0300

    refactor: move sut creation to a factory method

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -5,25 +5,42 @@ import {
 } from '../../presentation/protocols';
 import { LogControllerDecorator } from './log';
 
+interface SutTypes {
+  sut: LogControllerDecorator;
+  controllerStub: Controller;
+}
+
+const makeController = (): Controller => {
+  class ControllerStub implements Controller {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+      const httpResponse: HttpResponse = {
+        statusCode: 200,
+        body: {
+          name: 'Alexandre',
+        },
+      };
+      // eslint-disable-next-line arrow-parens
+      return new Promise(resolve => resolve(httpResponse));
+    }
+  }
+
+  return new ControllerStub();
+};
+
+const makeSut = (): SutTypes => {
+  const controllerStub = makeController();
+  const sut = new LogControllerDecorator(controllerStub);
+  return {
+    sut,
+    controllerStub,
+  };
+};
+
 describe('LogController Decorator', () => {
   test('Should call controller handle', async () => {
-    class ControllerStub implements Controller {
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-        const httpResponse: HttpResponse = {
-          statusCode: 200,
-          body: {
-            name: 'Alexandre',
-          },
-        };
-        // eslint-disable-next-line arrow-parens
-        return new Promise(resolve => resolve(httpResponse));
-      }
-    }
-
-    const controllerStub = new ControllerStub();
+    const { sut, controllerStub } = makeSut();
     const handleSpy = jest.spyOn(controllerStub, 'handle');
-    const sut = new LogControllerDecorator(controllerStub);
     const httpRequest = {
       body: {
         name: 'any_name',
