80d643019765f7cc03bd544b1994c9426ebc20db
= <alexandressoares383@gmail.com>
2023-04-05 19:20:10 -0300

    feat: ensure LogControllerDecorator calls controller with correct value

---
+++ src/main/decorators/log.spec.ts
@@ -0,0 +1,38 @@
+import {
+  Controller,
+  HttpRequest,
+  HttpResponse,
+} from '../../presentation/protocols';
+import { LogControllerDecorator } from './log';
+
+describe('LogController Decorator', () => {
+  test('Should call controller handle', async () => {
+    class ControllerStub implements Controller {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+        const httpResponse: HttpResponse = {
+          statusCode: 200,
+          body: {
+            name: 'Alexandre',
+          },
+        };
+        // eslint-disable-next-line arrow-parens
+        return new Promise(resolve => resolve(httpResponse));
+      }
+    }
+
+    const controllerStub = new ControllerStub();
+    const handleSpy = jest.spyOn(controllerStub, 'handle');
+    const sut = new LogControllerDecorator(controllerStub);
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+      },
+    };
+    await sut.handle(httpRequest);
+    expect(handleSpy).toHaveBeenCalledWith(httpRequest);
+  });
+});
