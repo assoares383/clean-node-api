cd5667f0e041c20501c95e03f7a333b2597480c2
= <alexandressoares383@gmail.com>
2023-04-06 18:18:45 -0300

    feat: ensure LoginController returns 400 if no password is provided

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,14 +1,27 @@
+/* eslint-disable arrow-parens */
 import { badRequest } from '../../helpers/http-helpers';
 import { Controller, HttpRequest, HttpResponse } from '../../protocols';
 import { MissingParamError } from '../../errors';
 
 export class LoginController implements Controller {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  // eslint-disable-next-line consistent-return
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    // eslint-disable-next-line arrow-parens
-    return new Promise(resolve =>
-      // eslint-disable-next-line implicit-arrow-linebreak
-      resolve(badRequest(new MissingParamError('email'))),
-    );
+    if (!httpRequest.body.email) {
+      return new Promise(
+        resolve =>
+          // eslint-disable-next-line implicit-arrow-linebreak
+          resolve(badRequest(new MissingParamError('email'))),
+        // eslint-disable-next-line function-paren-newline
+      );
+    }
+
+    if (!httpRequest.body.password) {
+      return new Promise(
+        resolve =>
+          // eslint-disable-next-line implicit-arrow-linebreak
+          resolve(badRequest(new MissingParamError('password'))),
+        // eslint-disable-next-line function-paren-newline
+      );
+    }
   }
 }
