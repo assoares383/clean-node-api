39be2d28bd3e7d581358ee72970791a9e960fcb1
= <alexandressoares383@gmail.com>
2023-04-05 22:20:34 -0300

    feat: ensure ExpressRouterAdapter returns the error message instead of the error name

--- src/main/adapters/express-route-adapter.ts
+++ src/main/adapters/express-route-adapter.ts
@@ -10,5 +10,11 @@ export const adaptRoute =
     };
 
     const httpResponse = await controller.handle(httpRequest);
-    res.status(httpResponse.statusCode).json(httpResponse.body);
+    if (httpResponse.statusCode === 200) {
+      res.status(httpResponse.statusCode).json(httpResponse.body);
+    } else {
+      res.status(httpResponse.statusCode).json({
+        error: httpResponse.body.message,
+      });
+    }
   };
