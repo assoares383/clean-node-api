562879f8ea5c522387f53bdd77cbb490f066a746
= <alexandressoares383@gmail.com>
2023-04-05 21:28:56 -0300

    refactor: remove duplicated code

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -3,27 +3,10 @@ import {
   HttpRequest,
   HttpResponse,
 } from '../../presentation/protocols';
-import { serverError } from '../../presentation/helpers/http-helpers';
+import { serverError, ok } from '../../presentation/helpers/http-helpers';
 import { LogControllerDecorator } from './log';
 import { LogErrorRepository } from '../../data/protocols/log-error-repository';
-
-const makeController = (): Controller => {
-  class ControllerStub implements Controller {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-      const httpResponse: HttpResponse = {
-        statusCode: 200,
-        body: {
-          name: 'Alexandre',
-        },
-      };
-      // eslint-disable-next-line arrow-parens
-      return new Promise(resolve => resolve(httpResponse));
-    }
-  }
-
-  return new ControllerStub();
-};
+import { AccountModel } from '../../domain/models/account';
 
 const makeLogErrorRepository = (): LogErrorRepository => {
   class LogErrorRepositoryStub implements LogErrorRepository {
@@ -35,6 +18,40 @@ const makeLogErrorRepository = (): LogErrorRepository => {
   }
 
   return new LogErrorRepositoryStub();
+};
+
+const makeFakeRequest = (): HttpRequest => ({
+  body: {
+    name: 'any_name',
+    email: 'any_email@gmail.com',
+    password: 'any_password',
+    passwordConfirmation: 'any_password',
+  },
+});
+
+const makeFakeAccount = (): AccountModel => ({
+  id: 'valid_id',
+  name: 'valid_name',
+  email: 'valid_email@gmail.com',
+  password: 'valid_password',
+});
+
+const makeController = (): Controller => {
+  class ControllerStub implements Controller {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+      // eslint-disable-next-line arrow-parens
+      return new Promise(resolve => resolve(ok(makeFakeAccount())));
+    }
+  }
+
+  return new ControllerStub();
+};
+
+const makeFakeServerError = (): HttpResponse => {
+  const fakeError = new Error();
+  fakeError.stack = 'any_stack';
+  return serverError(fakeError);
 };
 
 interface SutTypes {
@@ -61,56 +78,27 @@ describe('LogController Decorator', () => {
   test('Should call controller handle', async () => {
     const { sut, controllerStub } = makeSut();
     const handleSpy = jest.spyOn(controllerStub, 'handle');
-    const httpRequest = {
-      body: {
-        name: 'any_name',
-        email: 'any_email@mail.com',
-        password: 'any_password',
-        passwordConfirmation: 'any_password',
-      },
-    };
-    await sut.handle(httpRequest);
-    expect(handleSpy).toHaveBeenCalledWith(httpRequest);
+    await sut.handle(makeFakeRequest());
+    expect(handleSpy).toHaveBeenCalledWith(makeFakeRequest());
   });
 
   test('Should return the same result of the controller', async () => {
     const { sut } = makeSut();
-    const httpRequest = {
-      body: {
-        name: 'any_name',
-        email: 'any_email@mail.com',
-        password: 'any_password',
-        passwordConfirmation: 'any_password',
-      },
-    };
-    const httpResponse = await sut.handle(httpRequest);
-    expect(httpResponse).toEqual({
-      statusCode: 200,
-      body: {
-        name: 'Alexandre',
-      },
-    });
+    const httpResponse = await sut.handle(makeFakeRequest());
+    expect(httpResponse).toEqual(ok(makeFakeAccount()));
   });
 
   test('Should call LogErrorRepository with correct error if controller returns a server error', async () => {
     const { sut, controllerStub, logErrorRepositoryStub } = makeSut();
-    const fakeError = new Error();
-    fakeError.stack = 'any_stack';
-    const error = serverError(fakeError);
     const logSpy = jest.spyOn(logErrorRepositoryStub, 'log');
     jest
       .spyOn(controllerStub, 'handle')
       // eslint-disable-next-line arrow-parens
-      .mockReturnValueOnce(new Promise(resolve => resolve(error)));
-    const httpRequest = {
-      body: {
-        name: 'any_name',
-        email: 'any_email@mail.com',
-        password: 'any_password',
-        passwordConfirmation: 'any_password',
-      },
-    };
-    await sut.handle(httpRequest);
+      .mockReturnValueOnce(
+        // eslint-disable-next-line arrow-parens
+        new Promise(resolve => resolve(makeFakeServerError())),
+      );
+    await sut.handle(makeFakeRequest());
     expect(logSpy).toHaveBeenCalledWith('any_stack');
   });
 });
