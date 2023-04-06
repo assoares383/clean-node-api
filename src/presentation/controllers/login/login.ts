c787ef20dbf3e3c6fa704d3349a42542f3b250d1
= <alexandressoares383@gmail.com>
2023-04-06 19:48:51 -0300

    refactor: remove code duplication

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -22,35 +22,18 @@ export class LoginController implements Controller {
   // eslint-disable-next-line consistent-return
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      const requireFields = ['email', 'password'];
+
+      for (const field of requireFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field));
+        }
+      }
+
       const { email, password } = httpRequest.body;
-
-      if (!email) {
-        return new Promise(
-          resolve =>
-            // eslint-disable-next-line implicit-arrow-linebreak
-            resolve(badRequest(new MissingParamError('email'))),
-          // eslint-disable-next-line function-paren-newline
-        );
-      }
-
-      if (!password) {
-        return new Promise(
-          resolve =>
-            // eslint-disable-next-line implicit-arrow-linebreak
-            resolve(badRequest(new MissingParamError('password'))),
-          // eslint-disable-next-line function-paren-newline
-        );
-      }
-
       const isValid = this.emailValidator.isValid(email);
-
       if (!isValid) {
-        return new Promise(
-          resolve =>
-            // eslint-disable-next-line implicit-arrow-linebreak
-            resolve(badRequest(new InvalidParamError('email'))),
-          // eslint-disable-next-line function-paren-newline
-        );
+        return badRequest(new InvalidParamError('email'));
       }
 
       await this.authentication.auth(email, password);
