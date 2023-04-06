7c67f9bce5536cf40b0309ebe93622749369c11a
= <alexandressoares383@gmail.com>
2023-04-06 19:19:41 -0300

    feat: ensure LoginController returns 500 if EmailValidator throws

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,5 +1,5 @@
 /* eslint-disable arrow-parens */
-import { badRequest } from '../../helpers/http-helpers';
+import { badRequest, serverError } from '../../helpers/http-helpers';
 import {
   Controller,
   EmailValidator,
@@ -17,35 +17,39 @@ export class LoginController implements Controller {
 
   // eslint-disable-next-line consistent-return
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { email, password } = httpRequest.body;
+    try {
+      const { email, password } = httpRequest.body;
 
-    if (!email) {
-      return new Promise(
-        resolve =>
-          // eslint-disable-next-line implicit-arrow-linebreak
-          resolve(badRequest(new MissingParamError('email'))),
-        // eslint-disable-next-line function-paren-newline
-      );
-    }
+      if (!email) {
+        return new Promise(
+          resolve =>
+            // eslint-disable-next-line implicit-arrow-linebreak
+            resolve(badRequest(new MissingParamError('email'))),
+          // eslint-disable-next-line function-paren-newline
+        );
+      }
 
-    if (!password) {
-      return new Promise(
-        resolve =>
-          // eslint-disable-next-line implicit-arrow-linebreak
-          resolve(badRequest(new MissingParamError('password'))),
-        // eslint-disable-next-line function-paren-newline
-      );
-    }
+      if (!password) {
+        return new Promise(
+          resolve =>
+            // eslint-disable-next-line implicit-arrow-linebreak
+            resolve(badRequest(new MissingParamError('password'))),
+          // eslint-disable-next-line function-paren-newline
+        );
+      }
 
-    const isValid = this.emailValidator.isValid(email);
+      const isValid = this.emailValidator.isValid(email);
 
-    if (!isValid) {
-      return new Promise(
-        resolve =>
-          // eslint-disable-next-line implicit-arrow-linebreak
-          resolve(badRequest(new InvalidParamError('email'))),
-        // eslint-disable-next-line function-paren-newline
-      );
+      if (!isValid) {
+        return new Promise(
+          resolve =>
+            // eslint-disable-next-line implicit-arrow-linebreak
+            resolve(badRequest(new InvalidParamError('email'))),
+          // eslint-disable-next-line function-paren-newline
+        );
+      }
+    } catch (error) {
+      return serverError(error);
     }
   }
 }
