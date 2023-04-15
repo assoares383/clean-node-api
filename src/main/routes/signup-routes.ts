e23a3660f1ef122002b7bd1af6790fb81a243b16
= <alexandressoares383@gmail.com>
2023-04-15 16:30:32 -0300

    feat: add LoginValidationFactory

--- src/main/routes/signup-routes.ts
+++ src/main/routes/signup-routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { makeSignUpController } from '../factories/singup';
+import { makeSignUpController } from '../factories/signup/singup';
 import { adaptRoute } from '../adapters/express-route-adapter';
 
 export default (router: Router): void => {
