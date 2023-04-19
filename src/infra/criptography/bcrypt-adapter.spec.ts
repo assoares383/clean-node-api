0ccb4bc4aa5ba3c1dd7718965163a3a8594dc710
= <alexandressoares383@gmail.com>
2023-04-19 18:35:59 -0300

    feat: ensure BcryptAdapter returns true when compare succeeds

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -44,8 +44,13 @@ describe('Bcrypt Adapter', () => {
   test('Should call compare with correct values', async () => {
     const sut = makeSut();
     const compareSpy = jest.spyOn(bcrypt, 'compare');
-
     await sut.compare('any_value', 'any_hash');
     expect(compareSpy).toHaveBeenCalledWith('any_value', 'any_hash');
+  });
+
+  test('Should return true when compare succeeds', async () => {
+    const sut = makeSut();
+    const isValid = await sut.compare('any_value', 'any_hash');
+    expect(isValid).toBe(true);
   });
 });
