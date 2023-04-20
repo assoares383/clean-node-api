8b5ba46a2a90e4ee66a581ff05ea49bf27f3bc93
= <alexandressoares383@gmail.com>
2023-04-20 20:40:25 -0300

    test: add login route

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -1,8 +1,11 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import request from 'supertest';
-
+import { Collection } from 'mongodb';
+import { hash } from 'bcrypt';
 import app from '../config/app';
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper';
+
+let accountCollection: Collection;
 
 describe('Login Routes', () => {
   beforeAll(async () => {
@@ -14,7 +17,7 @@ describe('Login Routes', () => {
   });
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection('accounts');
+    accountCollection = await MongoHelper.getCollection('accounts');
     await accountCollection.deleteMany({});
   });
 
@@ -27,6 +30,24 @@ describe('Login Routes', () => {
           email: 'teste@teste.com.br',
           password: '123456',
           passwordConfirmation: '123456',
+        })
+        .expect(200);
+    });
+  });
+
+  describe('POST /login', () => {
+    test('Should return 200 on login', async () => {
+      const password = await hash('123456', 12);
+      await accountCollection.insertOne({
+        name: 'Teste',
+        email: 'teste@teste.com.br',
+        password,
+      });
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'teste@teste.com.br',
+          password: '123456',
         })
         .expect(200);
     });
