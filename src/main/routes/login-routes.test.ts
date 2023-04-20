8c8c0ec96e248bf7aa1d5535dacfd18db21db8e3
= <alexandressoares383@gmail.com>
2023-04-20 20:44:24 -0300

    test: login route returns 401 with invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -51,5 +51,14 @@ describe('Login Routes', () => {
         })
         .expect(200);
     });
+    test('Should return 401 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'teste@teste.com.br',
+          password: '123456',
+        })
+        .expect(401);
+    });
   });
 });
